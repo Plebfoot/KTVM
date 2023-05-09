5eea01078aaa3a208e3cb0c855ad07bf2618059b
Plebfoot <toxny.vaneck@gmail.com>
2023-05-09 21:28:57 +0200

    auth.php toegevoegd.

---
+++ resources/lang/en/aut.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'failed'   => 'Sorry, deze inloggegevens komen niet overeen met onze gegevens.',
+    'throttle' => 'Te veel inlogpogingen. Probeer het over :seconds seconden opnieuw.',
+    'password' => 'Sorry, het ingevoerde wachtwoord is onjuist. Probeer het opnieuw.',
+];
